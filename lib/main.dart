96472ad49a09c4f57a82120a176eaf4e1dc1b506
Mirna Hasan <mirnamhasan50@gmail.com>
2024-06-25 23:24:45 +0330

    name text

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,83 @@
+
+
 import 'package:flutter/material.dart';
 
 
-
 void main(){
-  runApp(AddNumbers());
+  runApp (const WritingName());
 }
-
-class AddNumbers extends StatefulWidget {
-  const AddNumbers({super.key});
+class WritingName extends StatefulWidget {
+  const WritingName({super.key});
 
   @override
-  State<AddNumbers> createState() => _AddNumbersState();
+  State<WritingName> createState() => _WritingNameState();
 }
 
-class _AddNumbersState extends State<AddNumbers> {
+class _WritingNameState extends State<WritingName> {
+ TextEditingController name = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-home: Scaffold(
-  appBar: AppBar(
-    title: Text("ADD NUMBERS"),
-    centerTitle: true,
-  ),
-  body:  Column( mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text("ADD TWO NUMBERS ", style: TextStyle(
-            fontSize: 30, fontWeight: FontWeight.bold, 
-            color: Colors.pinkAccent,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar( backgroundColor: Colors.blueGrey,
+          title: const Text("writting names", 
+        style: TextStyle(
+          fontSize: 40, 
+          fontWeight: FontWeight.bold,
 
-          ),), 
-         TextFormField(
-          decoration: InputDecoration( 
-      
-            hintText: "Enter The First Number", 
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(5),),
+        ),),
+        centerTitle: true,),
+        body: 
+         Center(
+          child: Form(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 30,
+                ),
+                const Text("WRITTING YOUR NAMES ", style: TextStyle(
+                  fontSize: 30, 
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueAccent,
+                ),),
+                const SizedBox(height: 20,),
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: TextFormField( 
+                  controller: name,
+                  
+                    decoration: InputDecoration( fillColor: const Color.fromARGB(255, 168, 203, 203),
+                    filled: true,
+                      labelText: " Enter Your Name,PLZ ",
+                      labelStyle: const TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.indigoAccent,
+                      ),
+                      focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(37),)
+                    ),
+                  ),
+                ),
+                MaterialButton( color: Colors.blue,
+                  onPressed: (){     
+             
+                  },
+
+               
+                
+               
+                ),
+                 Text("$name"),
+              ],
+            ),
           ),
-         ),
-         Container(height: 10,),
-          TextFormField(
-          decoration: InputDecoration(
-            hintText: "Enter The Second Number", 
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(5),),
-          ),
-         ),
-         MaterialButton( color: Colors.pink,
-          onPressed: (){}, 
-         child: const Text("ADD THE NUMBERS"),)
-
-        ],
-      )
-
-  ,
-),
-
+        ),
+      ),
     );
+    
   }
-}
+  
+  }
+
+  
